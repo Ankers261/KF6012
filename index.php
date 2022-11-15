efd98573fd9a911fdfe29f535ba9373371d2020d
Jason Ankers <ankers261@gmail.com>
2022-11-15 18:22:41 +0000

    Including autoloader in index file

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<php?
+<?php
+
+/**
+ * 
+ * 
+ * 
+ * @author Jason Ankers - W20004105
+ */
+
+header("Content-type: application/json; charset = UTF-8");
+header("Access-Control-Allow-Origin: *");
+
+
+//autoloader
+
+include 'autoloader.php';
+spl_autoload_register('autoloader');
+
