87936741c75becf10d20064642bb31772bc3733a
Jason Ankers <ankers261@gmail.com>
2022-12-21 23:52:40 +0000

    Adding rough JSON data and some headers
    Also added a check for the GET method

--- index.php
+++ index.php
@@ -6,13 +6,22 @@
  * 
  * @author Jason Ankers - W20004105
  */
-
-header("Content-type: application/json; charset = UTF-8");
+header("Content-Type: application/json; charset = UTF-8");
 header("Access-Control-Allow-Origin: *");
 
+if (!in_array($_SERVER['REQUEST_METHOD'], array("GET"))){
+    http_response_code(405);
+    $output['message'] = "Invalid: " . $_SERVER['REQUEST_METHOD'];
+} else {
+    $studentInfo = array(
+        "firstName" => "Jason",
+        "lastName" => "Ankers",
+        "studentID" => "W20004105"
+    );
 
-//autoloader
-
-include 'autoloader.php';
-spl_autoload_register('autoloader');
-
+    $output = array(
+        "student" => $studentInfo,
+        "docLink" => "LINK HERE"
+    );
+}
+ echo json_encode($output);
