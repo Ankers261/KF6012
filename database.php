ce4509a73819628a461fa543af867bf79722f7f8
Jason Ankers <ankers261@gmail.com>
2022-12-22 00:18:50 +0000

    Setting db connection with PDO

--- database.php
+++ database.php
@@ -1,2 +1,33 @@
 <?php
 
+/**
+ * 
+ * Connection script for CHIPlAY SQLite database
+ * 
+ * @author Jason Ankers - W20004105
+ */
+
+ class Database {
+
+    private $dbConn;
+
+    public function __construct($dbName) {
+        $this->setdbConn($dbName);
+    }
+
+    /**
+     * 
+     */
+
+    private function setdbConn($dbName) {
+        try {
+            $this->dbConn = new PDO('sqlite:'.$dbName);
+            $this->dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Error connecting to the database: " . $e->getMessage();
+            exit();
+        }
+    }
+
+    
+ }
