f2e15393a88e1cca40b45f17c06e9f05c2cbf33c
Jason Ankers <ankers261@gmail.com>
2022-12-23 19:07:06 +0000

    Slight upgrade to SQL statement
    Doesn't change outcome but is better practice

--- src/papers.php
+++ src/papers.php
@@ -11,7 +11,7 @@ class Papers extends Endpoint {
 
     protected function initialiseSQL() {
         $sqlQuery = "SELECT paper_id, title, award, 
-        abstract, name AS track_name, short_name AS sName
+        abstract, track.name AS track_name, track.short_name AS sName
         FROM paper JOIN track on (paper.track_id = track.track_id)"; //SQL for retreiving specific imformation on the papers endpoint goes here
         $this->setSQL($sqlQuery);
         $this->setSQLParams([]);
