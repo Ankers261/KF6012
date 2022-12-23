349514992acf4834f64108dc7d105dd8bc1add21
Jason Ankers <ankers261@gmail.com>
2022-12-23 18:55:53 +0000

    Adding SQL to return required fields
    Ran in the DB Browser tool and returns seemingly correct SQL.

--- src/papers.php
+++ src/papers.php
@@ -1,16 +1,18 @@
 <?php
 
 /**
+ * Papers endpoint
  * 
- * 
- * 
+ * @Author Jason Ankers - W20004105
  */
 
 class Papers extends Endpoint {
 
 
     protected function initialiseSQL() {
-        $sqlQuery = ""; //SQL for retreiving specific imformation on the papers endpoint goes here
+        $sqlQuery = "SELECT paper_id, title, award, 
+        abstract, name AS track_name, short_name AS sName
+        FROM paper JOIN track"; //SQL for retreiving specific imformation on the papers endpoint goes here
         $this->setSQL($sqlQuery);
         $this->setSQLParams([]);
     }
