840be945098749c5d0ce845519b0b6d62595b518
Jason Ankers <ankers261@gmail.com>
2022-12-23 19:01:57 +0000

    SQL corrected
    Join wasn't correct. Should be now

--- src/papers.php
+++ src/papers.php
@@ -12,7 +12,7 @@ class Papers extends Endpoint {
     protected function initialiseSQL() {
         $sqlQuery = "SELECT paper_id, title, award, 
         abstract, name AS track_name, short_name AS sName
-        FROM paper JOIN track"; //SQL for retreiving specific imformation on the papers endpoint goes here
+        FROM paper JOIN track on (paper.track_id = track.track_id)"; //SQL for retreiving specific imformation on the papers endpoint goes here
         $this->setSQL($sqlQuery);
         $this->setSQLParams([]);
     }
