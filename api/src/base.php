960927e951f41ba1eeef019e2c85fff68b305f0c
Jason Ankers <ankers261@gmail.com>
2022-12-23 19:59:01 +0000

    Adding constructor (May not be needed)

--- api/src/base.php
+++ api/src/base.php
@@ -9,12 +9,25 @@
 class Base extends Endpoint {
 
 
-    //THIS CLASS WILL BE DIFFERENT AS IT RETURNS THE HARD CODED DATA CURRENTLY IN THE INDEX FILE
-    // MOVE THIS DATA FROM THE INDEX FILE TO THIS ONE BY QUERYING THE DATABASE FOR THE FILM TITLE USING THE FUNCTION BELOW
-    //THEN FOLLOWING THE SAME PATTERN FROM THE ENDPOINT.PHP FILE's setData function, RETURN THE JSON DATA. I UNDERSTOOD HOW TO DO THIS LAST NIGHT 
-    //SO GOOD LUCK HOPEFULLY YOU CAN FIGURE IT OUT HAHA
+    public function __construct() {
+
+        
+
+        $database = new Database("db/chiplay.sqlite");
+
+        $this->initialiseSQL();
+
+        $data = $database->executeSQL($this->sqlQuery, $this->sqlParams);
+
+        $this->setData(array(
+            "length" => count($data),
+            "message" => "Successful",
+            "dataReturned" => $data
+        ));
+    }
+
     protected function initialiseSQL() {
-        $sqlQuery = ""; //SQL for retreiving specific imformation on the Base endpoint goes here
+        $sqlQuery = "";
         $this->setSQL($sqlQuery);
         $this->setSQLParams([]);
         
