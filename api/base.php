57920f6e8aec8336ab511c17aa0cddc3c68f7667
Jason Ankers <ankers261@gmail.com>
2022-12-24 13:43:49 +0000

    Trying different methods

--- api/base.php
+++ api/base.php
@@ -8,50 +8,17 @@
 
 class Base extends Endpoint {
 
-    //RETHINK HOW THIS CLASS WORKS AS IT SHOULDNT HAVE TO OVERWRITE CONSTRUCTOR
-    //ACTUALLY THIS DOESNT WORK SO DEFINITELY NEEDS RETHINKING
-    private $data;
-    private $sqlQuery;
-    private $sqlParams;
-
-    public function __construct() {
-
-        //Hardcoding student information
-        $studentInfo = array(
-            "firstName" => "Jason",
-            "lastName" => "Ankers",
-            "studentID" => "W20004105"
+        $hardcodedData = array(
+            "fName" => "Jason",
+            "sName" => "Ankers",
+            "studentID" => "W20004105",
+            "docPage" => "LINK HERE"
         );
 
-        //Hardcoding documentation
-        $base = array(
-            "student" => $studentInfo,
-            "docLink" => "LINK HERE FOR DOCUMENTATION PAGE" // This can be hardcoded - It will be created as part of TASK 4 so come back to this
-        );
-        
-        $database = new Database("db/chiplay.sqlite");
+        protected function initialiseSQL() {
+            $sqlQuery = "SELECT name FROM conference_information"; //SQL correct for now but will need a join to allow for paramters from paper table
+            $this->setSQL($sqlQuery);
+            $this->setSQLParams([]);
+        }
 
-        $this->initialiseSQL();
-
-        $data = $database->executeSQL($this->sqlQuery, $this->sqlParams);
-
-        //Combining SQL query for conference name with hardcoded data
-        $baseData = array(
-            "student" => $base,
-            "data" => $data
-        );
-
-        $this->setData(array(
-            "length" => count($baseData),
-            "message" => "Successful",
-            "dataReturned" => $baseData
-        ));
-    }
-
-    protected function initialiseSQL() {
-        $sqlQuery = "SELECT name FROM conference_information";
-        $this->setSQL($sqlQuery);
-        $this->setSQLParams([]);
-        
-    }
 }
