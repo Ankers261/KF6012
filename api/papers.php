efd564b3227fcc26bce94d835984ba74220c5166
Jason Ankers <ankers261@gmail.com>
2022-12-26 13:49:38 +0000

    Adding the parameter checking (NOT WORKING)

--- api/papers.php
+++ api/papers.php
@@ -13,7 +13,34 @@ class Papers extends Endpoint {
         $sqlQuery = "SELECT paper_id, title, award, 
         abstract, track.name AS track_name, track.short_name AS sName
         FROM paper JOIN track on (paper.track_id = track.track_id)"; //SQL for retreiving specific imformation on the papers endpoint goes here
+
+        $sqlParams = [];
+
+        if (filter_has_var(INPUT_GET, 'track')) {
+            if(isset($sqlWhere)) {
+                $sqlWhere .= " AND track.short_name LIKE :track"; //If where is already set, another where is set so more than 1 track can be returned
+            } else {
+                $sqlWhere = " WHERE track.short_name LIKE :track";//No where is set so 1 track name will be returned
+            }
+            
+            $sqlParams['track'] = $_GET['track'];
+        } else {
+            echo "NOPE";
+        }
+
+        if(isset($sqlWhere)) {
+            $sqlQuery .= $sqlWhere; //SO FAR THIS IS WHERE THE 
+                                    //PROGRAM GETS TO AND QUERY IS AS 
+                                    //EXPECTED THEREFORE IT IS SOMETHING WRONG WITH AFTER THIS
+        } else {
+            echo "NONO";
+        }
+
+
+
         $this->setSQL($sqlQuery);
-        $this->setSQLParams([]);
+        $this->setSQLParams($sqlParams);
+
+
     }
 }
