b0b7259d8b7735d0e80a359f439ce94ec8687857
Jason Ankers <ankers261@gmail.com>
2022-12-30 21:48:29 +0000

    Adding parameter for papers on author endpoint

--- api/authors.php
+++ api/authors.php
@@ -10,9 +10,29 @@ class Authors extends Endpoint {
 
 
     protected function initialiseSQL() {
-        $sqlQuery = "SELECT author_id, first_name, middle_initial, last_name
-        FROM author"; //SQL correct for now but will need a join to allow for paramters from paper table
+        $sqlQuery = "SELECT author.author_id, first_name, middle_initial, last_name
+        FROM author 
+        JOIN paper_has_author on (author.author_id = paper_has_author.author_id)
+        JOIN paper on (paper_has_author.paper_id = paper.paper_id)"; 
+
+        $sqlParams = [];
+
+        if(filter_has_var(INPUT_GET, 'paper_id')) {
+            if(isset($sqlWhere)) {
+                $sqlWhere .= " AND paper.paper_id = :paper_id";
+            } else {
+                $sqlWhere = "WHERE paper.paper_id = :paper_id";
+            }
+
+            $sqlParams['paper_id'] = $_GET['paper_id'];
+
+            if(isset($sqlWhere)) {
+                $sqlQuery .= $sqlWhere;
+            }
+        }
+
+
         $this->setSQL($sqlQuery);
-        $this->setSQLParams([]);
+        $this->setSQLParams($sqlParams);
     }
 }
