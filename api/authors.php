e139693511761e1d247dbb73eb04dab3d183b594
Jason Ankers <ankers261@gmail.com>
2023-01-03 22:25:16 +0000

    DocComments

--- api/authors.php
+++ api/authors.php
@@ -1,14 +1,26 @@
 <?php
 
 /**
+ * Authors endpoint.
  * 
- * 
- * 
+ * @author Jason Ankers - W20004105
  */
 
 class Authors extends Endpoint {
 
 
+    /**
+     * Sets the SQL required by the Authors endpoint.
+     * 
+     * Overrides initialiseSQL function from parent class. 
+     * 
+     * SQL specifies columns to return alongside joining 
+     * the paper_has_author table and the paper table.
+     * The purpose of this is not for the information selected directly, 
+     * but for the parameters that will alter
+     * the data that is returned from the database.
+     * 
+     */
     protected function initialiseSQL() {
         $sqlQuery = "SELECT author.author_id, first_name, middle_initial, last_name
         FROM author 
@@ -27,8 +39,10 @@ class Authors extends Endpoint {
 
 
             if(isset($sqlWhere)) {
+                //If a where clause is already set, another where clause is added so that more than one parameter can be used
                 $sqlWhere .= " AND paper.paper_id = :paper_id";
             } else {
+                //No where clause is set so only one parameter used
                 $sqlWhere = "WHERE paper.paper_id = :paper_id";
             }
 
@@ -44,6 +58,7 @@ class Authors extends Endpoint {
         $this->setSQLParams($sqlParams);
     }
 
+    //Establishes the paramter name array for this endpoint
     protected function validEndpointParams() {
         return ['paper_id'];
     }
