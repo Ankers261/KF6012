bd171884bf407ceae18c9261b868a8705c9a30f9
Jason Ankers <ankers261@gmail.com>
2023-01-03 22:25:08 +0000

    DocComments

--- api/clienterror.php
+++ api/clienterror.php
@@ -1,7 +1,11 @@
 <?php
 /**
  * Handles client errors
+ * 
  * Child of Endpoint class as it is an endpoint that deals with errors e.g. 404, 405.
+ * 
+ * @author Jason Ankers - w20004105
+ * @author John Rooksby
  */
 
 class ClientError extends Endpoint {
