3d49c6064c8a210d694756bfb49d1144585c6dc5
Jason Ankers <ankers261@gmail.com>
2023-01-03 22:25:00 +0000

    DocComments

--- api/base.php
+++ api/base.php
@@ -1,9 +1,13 @@
 <?php
 
 /**
+ * Base endpoint
  * 
+ * Unlinke other endpoint classes, this has to 
+ * override the Endpoint class constructor to
+ * be able to hardcode some necessary data.
  * 
- * 
+ * @author Jason Ankers - W20004105
  */
 
 class Base extends Endpoint {
@@ -14,7 +18,7 @@ class Base extends Endpoint {
         $this->setSQLParams([]);
     }
 
-    
+    //Overriding the constructor of Endpoint class
     public function __construct() {
         $database = new Database("db/chiplay.sqlite");
 
@@ -22,6 +26,7 @@ class Base extends Endpoint {
 
         $data = $database->executeSQL($this->sqlQuery, $this->sqlParams);
 
+        //Hardcoded student data
         $student = array(
             "name" => "Jason Ankers",
             "stuID" => "W20004105"
