14463ee71111f2e5fd1b11435e8a227d9bc067c3
Jason Ankers <ankers261@gmail.com>
2023-01-09 18:57:08 +0000

    fetching API and returning basic data for Interactivity page

--- assignment-app/src/Interactivity.js
+++ assignment-app/src/Interactivity.js
@@ -1,11 +1,43 @@
 import Footer from './Footer';
 import Navbar from './Navbar';
+import { useEffect, useState } from 'react';
 
 function Interactivity() {
+    
+    const [papers, setPapers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect( () => {
+        fetch("http://unn-w20004105.newnumyspace.co.uk/webYear3/assignment/api/papers?track=Interactivity")
+        .then(
+           (response) => response.json()
+        )
+        .then(
+            (json) => {
+                setPapers(json.dataReturned)
+                setLoading(false)
+            }
+        )
+        .catch(
+            (e) => {
+                console.log(e.message)
+            }
+        )
+    }, []);
+
+
+    const allPapers = papers.map(
+        (value, key) => <p key = {key}>
+            <div>Title: {value.title}</div>  <div>Award: {value.award}</div> <div>Abstract: {value.abstract}</div>
+        </p>
+    )
+    
     return (
       <div className="Interactivity">
         <h2>Interactivity</h2>
         <Navbar/>
+        {loading && <p>Loading...</p>}
+        {allPapers}
         <Footer/>
       </div>
     );
