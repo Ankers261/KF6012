74218265d0c05d33d0bfd0972c66a04a6499e4c1
Jason Ankers <ankers261@gmail.com>
2023-01-11 11:58:42 +0000

    More css changes

--- assignment-app/src/Home.js
+++ assignment-app/src/Home.js
@@ -1,13 +1,13 @@
-import Navbar from './Navbar';
 import Footer from './Footer';
+import './Home.css';
 import homeImage from './home-image.jpg';
 
 function Home() {
   return (
     <div className="Home">
         <h1>CHI PLAY '21</h1>
-        <p>Overview text goes here...</p>
         <img src = {homeImage} alt = "Conference hall"/>
+        <p>Overview text goes here...</p>
         <Footer/>
     </div>
   );
