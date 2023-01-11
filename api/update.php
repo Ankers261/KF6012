9e7756ee3a4b089d0352ecec11997fd31e490151
Jason Ankers <ankers261@gmail.com>
2023-01-11 18:04:42 +0000

    adding token validation

--- api/update.php
+++ api/update.php
@@ -1,5 +1,7 @@
 <?php
 
+use FirebaseJWT\JWT;
+use FirebaseJWT\Key;
 
 /**
  * 
@@ -11,6 +13,7 @@ class Update extends Endpoint {
     public function __construct() {
 
         $this->validateRequestMethod("POST");
+        $this->validateToken();
 
         http_response_code(503);
 
@@ -22,4 +25,26 @@ class Update extends Endpoint {
     }
 
 
+    private function validateToken() {
+        $secretKey = SECRET;
+
+        $allHeaders = getallheaders();
+        $authorizationHeader = "";
+
+        //Checking for capitalisation
+        if (array_key_exists('Authorization', $allHeaders)) {
+            $authorizationHeader = $allHeaders['Authorization'];
+        } elseif (array_key_exists('authorization', $allHeaders)) {
+            $authorizationHeader = $allHeaders['authorization'];
+        }
+
+        //Checking for bearer token - will be thrown if authorizationHeader is blank
+        if (substr($authorizationHeader, 0, 7) != 'Bearer ') {
+            throw new ClientErrorException("Bearer token required", 401);
+        }
+
+        $jwt = trim(substr($authorizationHeader, 7));
+
+        $decoded = JWT::decode($jwt, new Key($secretKey, 'HS256'));
+    }
 }
