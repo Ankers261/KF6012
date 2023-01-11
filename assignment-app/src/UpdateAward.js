18a685b5a26473519aa8d2a2acc11f2f1b2f180d
Jason Ankers <ankers261@gmail.com>
2023-01-11 21:14:33 +0000

    update awards component

---
+++ assignment-app/src/UpdateAward.js
@@ -0,0 +1,50 @@
+
+function UpdateAward(props) {
+
+    function refreshPage() {
+        setTimeout(() => {
+            window.location.reload(false);
+        }, 250);
+    }
+ 
+    const handleSelect = (event) => {
+        const formData = new FormData();
+        formData.append('award', event.target.value);
+        formData.append('paper_id', props.paper.paper_id);
+
+        const token = localStorage.getItem('token');
+
+        fetch("http://unn-w20004105.newnumyspace.co.uk/webYear3/assignment/api/update",
+            {
+                method: 'POST',
+                headers: new Headers({ "Authorization": "Bearer " + token }),
+                body: formData
+            })
+            .then(
+                (response) => response.text()
+            )
+            .then(
+                (json) => {
+                    console.log(json)
+                })
+            .catch(
+                (e) => {
+                    console.log(e.message)
+                })
+
+        refreshPage();
+    }
+
+
+
+    return (
+      <div>
+        <h3>{props.paper.title}</h3>
+        <select value = {props.paper.award ? "true" : ""} onChange = {handleSelect}>
+            <option value = "">Not awarded</option>
+            <option value = "true">Awarded</option>
+        </select>
+      </div>
+    )
+  }
+  export default UpdateAward;
