bc256c38c48a44cd22b446dd744c21ec1963b1fd
Jason Ankers <ankers261@gmail.com>
2023-01-11 11:38:53 +0000

    Addign CSS to app.js and navbar comp

--- assignment-app/src/App.js
+++ assignment-app/src/App.js
@@ -9,7 +9,9 @@ import {Route, Routes} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <div className='navbar'>
         <Navbar/>
+      </div>
         <Routes>
           <Route path ="/" element = {<Home/>}/>
           <Route path = "/Papers" element = {<Papers/>} />
--- assignment-app/src/Navbar.js
+++ assignment-app/src/Navbar.js
@@ -1,17 +1,18 @@
 import {Link} from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
     return (
       <div className="Navbar">
         <ul>
-          <li><Link to = "/">Home</Link></li>
-          <li><Link to = "/Papers">Papers</Link></li>
-          <li><Link to = "/Interactivity">Interactivity</Link></li>
-          <li><Link to = "/Fullpapers">Fullpapers</Link></li>
-          <li><Link to = "/Wip">Wip</Link></li>
-          <li><Link to = "/Competition">Competition</Link></li>
-          <li><Link to = "/Doctoral">Doctoral</Link></li>
-          <li><Link to = "/Rapid">Rapid</Link></li>
+          <li><Link className = 'link' to = "/">Home</Link></li>
+          <li><Link className = 'link' to = "/Papers">Papers</Link></li>
+          <li><Link className = 'track' to = "/Interactivity">Interactivity</Link></li>
+          <li><Link className = 'track' to = "/Fullpapers">Fullpapers</Link></li>
+          <li><Link className = 'track' to = "/Wip">Wip</Link></li>
+          <li><Link className = 'track' to = "/Competition">Competition</Link></li>
+          <li><Link className = 'track' to = "/Doctoral">Doctoral</Link></li>
+          <li><Link className = 'track' to = "/Rapid">Rapid</Link></li>
         </ul>
       </div>
     );
